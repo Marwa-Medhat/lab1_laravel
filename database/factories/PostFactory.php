7e5d0772780a8e143ffa9ceb4cf74e4b8be6e840
Marwa Medhat <eng.marwamedhat2020@gmail.com>
2021-03-21 23:20:35 +0200

    seed fix

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class PostFactory extends Factory
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use App\Models\Post;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -16,6 +21,6 @@ class DatabaseSeeder extends Seeder
         // php artisan db:seed
         // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory(10)->create();
-        //  \App\Models\Post::factory(500)->create();
+         \App\Models\Post::factory(100)->create();
     }
 }
